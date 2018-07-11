6ac1767c236729a1baa93a84f74270832dad6e4a
haseeb <haseeb@yallamotor.com>
2018-07-11 10:35:58 +0500

    Comments and user profile

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :players
   resources :posts
+  resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
