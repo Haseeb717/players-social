17e7ac670b28ae2ea232e51b68412bdcc3b2415c
haseeb <haseeb@yallamotor.com>
2018-07-14 12:50:03 +0500

    Feedback 3

--- db/migrate/20180714061413_add_spam_to_users.rb
+++ db/migrate/20180714061413_add_spam_to_users.rb
@@ -1,6 +1,6 @@
 class AddSpamToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :spam, :boolean
-    add_column :posts, :spam, :boolean
+    add_column :users, :spam, :boolean,:default=>false
+    add_column :posts, :spam, :boolean,:default=>false
   end
 end
