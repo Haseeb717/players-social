b4ac622a46bf9f4a7e3cc348a547f7d29a11f4d9
haseeb <haseeb@yallamotor.com>
2018-08-11 19:31:58 +0500

    Dependent Destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :player, optional: true
-  has_many :tags
-  has_many :comments
-  has_many :tag_users
+  has_many :tags,:dependent => :destroy
+  has_many :comments,:dependent => :destroy
+  has_many :tag_users,:dependent => :destroy
   has_attached_file :image, styles: { small: "64x64", med: "100x100", profile: "140x140" }
 	validates_attachment_content_type :image, 
                                     :content_type => /^image\/(png|gif|jpeg|jpg)/,
